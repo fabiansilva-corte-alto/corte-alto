e1109a0fcdc53af3b457596cfc31fb25a9955426
fabiansilva-corte-alto <fabian.silva.veinte@gmail.com>
2026-09-08 12:33:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    alguien abre la app.
    ============================================================ */
 
-const CACHE_VERSION = 'v22';
+const CACHE_VERSION = 'v23';
 const CACHE_NAME = `corte-alto-${CACHE_VERSION}`;
 
 /* Archivos que se guardan para que la app funcione más rápido
@@ -32,6 +32,7 @@ const ARCHIVOS_CACHE = [
   './comparador-volumenes.html',
   './controles-mininco.html',
   './huinche.html',
+  './cuadrilla-diaria.html',
   './manifest.json',
   './logo.png'
 ];
