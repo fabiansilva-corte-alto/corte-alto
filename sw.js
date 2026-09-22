4fb177eb4ad5b76dabd9ce1467aabf42cc58edd2
fabiansilva-corte-alto <fabian.silva.veinte@gmail.com>
2026-09-22 11:20:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    alguien abre la app.
    ============================================================ */
 
-const CACHE_VERSION = 'v23.1';
+const CACHE_VERSION = 'v23.2';
 const CACHE_NAME = `corte-alto-${CACHE_VERSION}`;
 
 /* Archivos que se guardan para que la app funcione más rápido
